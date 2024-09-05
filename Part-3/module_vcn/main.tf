cb5a7841ad5aaec2ea59bc9b54be4d5a2be72114
cmschetan <89693887+cmschetan@users.noreply.github.com>
2024-09-05 12:47:43 +0530

    Delete Part-3 directory

--- Part-3/module_vcn/main.tf
+++
@@ -1,6 +0,0 @@
-resource "oci_core_vcn" "vcn" {
-  compartment_id = var.compartment_id
-  for_each = {for idx, cidr in var.vcn_cidr : idx => cidr}
-  cidr_block = each.value
-  display_name = var.display_name[each.key]
-}
--- Part-3/module_vcn/output.tf
+++
@@ -1,4 +0,0 @@
-output "vcn_ids" {
-  value = [for vcn in oci_core_vcn.vcn : vcn.id]
-    description = "VCN ID of the vcn"
-}
